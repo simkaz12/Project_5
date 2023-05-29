82e26a712d765c9826fd01bee414b10fb2d5fd69
Simonas Kazlauskas <sksimkaz8@gmail.com>
2023-05-29 15:06:21 +0300

    numbers

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+
+
+esminiai konceptai:
+1)kintamieji - informacijos atsiminimas
+2)if - info lyginimas
+3)loop - operaciju kartojimai n kartu 
+4) function - pasikartojancios logikos aprasymas
+
+
+
+*/
---
+++ js/02-numbers.js
@@ -0,0 +1,48 @@
+
+
+console.clear();
+
+// numbers
+// int/float
+// teigiami/neigiami
+// begalybes ir NaN
+
+// aritmetiniai operatoriai: +, -, *, /
+// Salia matematikos
+// kintamuju inicijavimo budai:
+//const - default
+//let - naudoti tik jei reikia keisti reiksmes
+//var - !!!senove!!! nenaudoti
+
+console.log(1);
+console.log(2);
+console.log(3.14);
+console.log(2.72);
+console.log(Infinity);
+console.log(-Infinity);
+console.log(NaN);
+
+console.log(2+2, 4);
+console.log(0.1+0.2, 0.3);
+
+console.clear();
+// Kintamieji: konstantos ir kintamieji
+//kintamieji
+
+const amzius = 20;
+console.log(amzius);
+
+const saldainiuKiekis = 19;
+
+let pinigine = 0;
+console.log(pinigine);
+
+pinigine = 5;
+console.log(pinigine);
+
+pinigine = 4+3;
+console.log(pinigine);
+
+
+
+
