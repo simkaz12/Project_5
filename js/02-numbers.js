8cab173ea1b0232725063c3a85ad7892e7c3fbf0
Simonas Kazlauskas <sksimkaz8@gmail.com>
2023-05-30 13:25:44 +0300

    skaiciai

--- js/02-numbers.js
+++ js/02-numbers.js
@@ -7,7 +7,7 @@ console.clear();
 // teigiami/neigiami
 // begalybes ir NaN
 
-// aritmetiniai operatoriai: +, -, *, /
+// aritmetiniai operatoriai: +, -, *, /, **, ++, --
 // Salia matematikos
 // kintamuju inicijavimo budai:
 //const - default
@@ -42,6 +42,102 @@ console.log(pinigine);
 
 pinigine = 4+3;
 console.log(pinigine);
+
+console.clear();
+
+let driezas = 1;
+
+driezas = 5 + driezas;
+
+console.log(driezas);
+
+console.clear();
+ let rezultatas = 10;
+
+ rezultatas += 20;
+ rezultatas -= 4;
+ rezultatas /= 13;
+ console.log(rezultatas);
+
+ console.clear();
+
+let x = 0;
+x += 1;
+console.log(x);
+x++;                                // x + 1;
+console.log(x);
+
+
+console.clear();
+
+/*
+console.log(x++)            pirma x tada + 1
+console.log(++x)            pirma + 1 tada x
+*/
+
+const a = 8;
+const b = 9;
+const c = 10;
+
+const d = a + b + c;
+console.log(d);
+
+console.clear();
+
+const p1 = 10;
+const p2 = 10;
+const p3 = 10;
+const p4 = 10;
+const p5 = 10;
+const vidurkis = (p1 + p2 + p3 + p4 + p5) / 5;
+console.log(vidurkis)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
