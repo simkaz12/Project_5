d26aebb706dc90beadafdd7f20306a7ca6b205d5
Simonas Kazlauskas <sksimkaz8@gmail.com>
2023-05-30 15:45:06 +0300

    namu darbai

--- js/03-string.js
+++ js/03-string.js
@@ -14,7 +14,94 @@ console.log(vardas);
 
 console.clear();
 
+// \n - new line
+// \r - eilutes pradzia
+// \t - tab
+// \r\n
+
+const kabute3 = ``;
+// const name = "Martynas";
+// const age = 99;
+// ...
+// const martis = `Sveiki, mano vardas ${name}, man yra ${age} metai ir man labai patinka ${iLike1}, ${iLike2} ir ${iLike3}!`
+
+console.clear();
+
+
+
+//const lentele1 = x " * = 1;\r\n";
 
 
 
 
+
+
+
+
+console.clear();
+
+const x = 7;
+const a = 1;
+const b = 2;
+const c = 3;
+const d = 4;
+const e = 5;
+const f = 6;
+const g = 7;
+const h = 8;
+const i = 9;
+const j = 10;
+
+const Result1 = x * a;
+const Result2 = x * b;
+const Result3 = x * c;
+const Result4 = x * d;
+const Result5 = x * e;
+const Result6 = x * f;
+const Result7 = x * g;
+const Result8 = x * h;
+const Result9 = x * i;
+const Result10 = x * j;
+
+const lentele1 = x + " * " + a + " = " + Result1 + " \r\n\ " + x + " * " + b + " = " + Result2 + " \r\n\ " + x + " * " + c + " = " + Result3 + " \r\n\ " + x + " * " + d + " = " + Result4 + " \r\n\ " + x + " * " + e + " = " + Result5 + " \r\n\ " + x + " * " + f + " = " + Result6 + " \r\n\ " + x + " * " + g + " = " + Result7 + " \r\n\ " + x + " * " + g + " = " + Result7 + " \r\n\ " + x + " * " + h + " = " + Result8 + " \r\n\ " + x + " * " + i + " = " + Result9 + " \r\n\ " + x + " * " + j + " = " + Result10 + " \r\n\ ";
+ 
+    
+console.log(lentele1);
+
+
+const xx = 7;
+const a2 = 1;
+const b3 = 2;
+const c4 = 3;
+const d5 = 4;
+const e6 = 5;
+const f7 = 6;
+const g8 = 7;
+const h9 = 8;
+const i9 = 9;
+const j10 = 10;
+
+const Result11 = xx * a2;
+const Result22 = xx * b3;
+const Result33 = xx * c4;
+const Result44 = xx * d5;
+const Result55 = xx * e6;
+const Result66 = xx * f7;
+const Result77 = xx * g8;
+const Result88 = xx * h9;
+const Result99 = xx * i9;
+const Result101 = xx * j10;
+
+const lentele2 = `${xx} * ${a2} = ${Result11}
+${xx} * ${b3} = ${Result22}
+${xx} * ${c4} = ${Result33}
+${xx} * ${d5} = ${Result44}
+${xx} * ${e6} = ${Result55}
+${xx} * ${f7} = ${Result66}
+${xx} * ${g8} = ${Result77}
+${xx} * ${h9} = ${Result88}
+${xx} * ${i9} = ${Result99}
+${xx} * ${j10} = ${Result101}`;
+
+
+console.log(lentele2);
