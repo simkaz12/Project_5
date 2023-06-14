66733e00e0b0d3a6a65693c3e959e525cafeaceb
Simonas Kazlauskas <sksimkaz8@gmail.com>
2023-06-14 14:42:59 +0300

    arrow function

--- js/11-arrayMethod.js
+++ js/11-arrayMethod.js
@@ -1,3 +1,23 @@
+/*
+butini:
+at
+flat
+includes
+indexOf
+join
+push
+reduce
+reverse
+slice
+
+
+
+filter
+map
+sort
+forEach
+*/
+
 console.clear();
 
 function includes(list, needle) {
@@ -59,6 +79,76 @@ Number.prototype.length = function length(number) {
 console.log((5).length());
 console.log((55).length());
 console.log((5555).length());
+
+
+
+// for
+    // for-of
+    // for-in
+    // while
+    // do-while
+
+    //map
+    //foreach
+    //sort
+    //reduce
+    //filter
+    //find
+
+
+
+console.clear();
+
+const masyvas = [10, 2, 8, 4, 6];
+
+console.log(masyvas.at(1));
+console.log(masyvas.at(-1));
+
+const double = [5, 4, 7, [4, 7, [6, 5], 8], 7, [1, 2], 0];
+
+console.log(double.flat());
+console.log(double.flat().flat());
+
+console.log(masyvas.join());
+console.log(masyvas.join('-'));
+console.log(masyvas.join(' '));
+console.log(masyvas.join(' -==- '));
+
+masyvas.push(10);
+console.log(masyvas);
+masyvas.pop();
+console.log(masyvas);
+
+const last = masyvas.shift();
+console.log(masyvas, last);
+
+const last1 = masyvas.unshift();
+console.log(masyvas, last1);
+
+masyvas.reverse();
+console.log(masyvas);
+
+const x = masyvas.slice(1,2);
+console.log(x);
+
+const y = masyvas.splice(2);
+console.log(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ js/12-arrow.js
@@ -0,0 +1,41 @@
+console.clear();
+const pirmas=7;
+const antras=5;
+
+function sum(a,b) {
+    return a + b;
+}
+
+const diff = function (a,b) {
+    return a-b;
+}
+const multi = (a,b) => {
+    return a*b;
+}
+// jei logikos bloke yra 1 statement galima nerasyti: {return}
+const div = (a,b) => a/b;
+
+// jeigu parametru bloke yra tik 1 parametras tai galima nerasyti: ()
+const square = a => a*a;
+
+
+//--------------------------------
+
+const abr = (vardas, pavarde) => vardas[0] + pavarde[0];
+
+console.log(abr('vardenis', 'pavardenis'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
